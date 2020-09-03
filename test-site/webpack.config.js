19613a3d79c43c24489a6c420c77f9312c0f2206
Rob Nobile <robert.nobile@gmail.com>
2020-09-03 12:10:10 -0400

    Begin favicon review

--- test-site/webpack.config.js
+++ test-site/webpack.config.js
@@ -21,37 +21,40 @@ module.exports = {
   entry: path.resolve(__dirname, './client/src/index.js'),
   module: {
     rules: [
-    {
-      test: /.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-react', '@babel/preset-env'],
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
         },
       },
-    },
-    {
-      test: /.(css|scss)$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    },
-    {
-      test: /.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
-      use: [
-        'file-loader',
-      ],
-    },
-  ]},
-resolve: {
-	extensions: ['.js', '.jsx', ]},
+      {
+        test: /.(css|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx',]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './client/src/index.html',
+      // favicon: "./client/assets/favicon.ico"
     }),
-    new CleanWebpackPlugin(), 
+    new CleanWebpackPlugin(),
   ]
 }
