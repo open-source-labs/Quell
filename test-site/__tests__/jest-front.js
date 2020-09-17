327eb3f6560997685a0b7baafd6f8eb7342b3c71
Rob Nobile <robert.nobile@gmail.com>
2020-09-17 16:48:15 -0400

    Initialize test files

--- test-site/__tests__/jest-front.js
+++ test-site/__tests__/jest-front.js
@@ -1,5 +1,89 @@
 import sum from '../functions/sum';
+import createQueryObj from '../functions/createQueryObj';
+import createQueryStr from '../functions/createQueryStr';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+describe('Query Construct', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+
+  // const map = { 
+  //   countries: 'Country',
+  //   country: 'Country',
+  //   citiesByCountryId: 'City',
+  //   cities: 'City'
+  // };
+  
+  // const fieldsMap = { cities: 'City' };
+
+  // // prototype, map, collection
+  // describe('buildArray', () => {
+  //   expect(buildArray(
+  //     arg1,
+  //     arg2,
+  //     arg3)).toEqual
+  // });
+
+  describe('createQueryObj', () => {
+    let prototype;
+
+    beforeEach(() => {
+      prototype = {
+        countries: {
+          id: true, 
+          name: true, 
+          capital: true, 
+          cities: {
+            country_id: true, 
+            id: true, 
+            name: true, 
+            population: true
+          },
+        }};
+    });
+
+    it('inputs prototype w/ all true and outputs empty object', () => {
+      expect(createQueryObj(prototype)).toEqual({});
+    });
+
+    it('inputs prototype w/ true/false for only scalar types and outputs object for false only', () => {
+      prototype.countries.id = false;
+      prototype.countries.name = false;
+      prototype.countries.capital = false;
+      expect(createQueryObj(prototype)).toEqual({ countries: [ 'id', 'name', 'capital' ] });
+    });
+    
+    it('inputs prototype w/ true/false for only object types and outputs object for false only', () => {
+      prototype.countries.cities.country_id = false;
+      prototype.countries.cities.id = false;
+      prototype.countries.cities.name = false;
+      prototype.countries.cities.population = false;
+      expect(createQueryObj(prototype)).toEqual({ countries: [{ cities: ['country_id', 'id', 'name', 'population'] }] });
+    });
+    
+    it('inputs prototype w/ true/false for both scalar & object types and outputs object for all false', () => {
+      prototype.countries.id = false;
+      prototype.countries.name = false;
+      prototype.countries.cities.country_id = false;
+      prototype.countries.cities.population = false;
+      expect(createQueryObj(prototype)).toEqual({ countries: ['id', 'name', { cities: ['country_id', 'population'] }] });
+    });
+  });
+
+  describe('createQueryStr', () => {
+    const scalar = { countries: [ 'id', 'name', 'capital' ] };
+    it('inputs query object w/ only scalar types and outputs GCL query string', () => {
+      expect(createQueryStr(scalar)).toMatch( ' { countries { id name capital  }  } ' );
+    });
+    
+    const object = { countries: [{ cities: ['country_id', 'id', 'name', 'population'] }] }
+    it('inputs query object w/ only object types and outputs GCL query string', () => {
+      expect(createQueryStr(object)).toMatch( ' { countries { cities { country_id id name population  }  }  } ' );
+    });
+    
+    const both = { countries: ['id', 'name', { cities: ['country_id', 'population'] }] }
+    it('inputs query object w/ both scalar & object types and outputs GCL query string', () => {
+      expect(createQueryStr(both)).toMatch( ' { countries { id name cities { country_id population  }  }  } ' );
+    });
+  });
 });
---
+++ test-site/functions/createQueryObj.js
@@ -0,0 +1,37 @@
+/**
+ createQueryObj takes in a map of field(keys) and true/false(values) creating an query object containing the fields (false) missing from cache. This will then be converted into a GQL query string in the next step.
+ */
+
+function createQueryObj(map) {
+  const output = {};
+  // !! assumes there is only ONE main query, and not multiples !!
+  for (let key in map) {
+    const reduced = reducer(map[key]);
+    if (reduced.length > 0) {
+      output[key] = reduced;
+    }
+  }
+
+  function reducer(obj) {
+    const fields = [];
+
+    for (let key in obj) {
+      // For each property, determine if the property is a false value...
+      if (obj[key] === false) fields.push(key);
+      // ...or another object type
+      if (typeof obj[key] === 'object') {
+        let newObjType = {};
+        let reduced = reducer(obj[key]);
+        if (reduced.length > 0) {
+          newObjType[key] = reduced;
+          fields.push(newObjType);
+        }
+      }
+    }
+
+    return fields;
+  }
+  return output;
+};
+
+export default createQueryObj
