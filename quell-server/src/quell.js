3f40330ff2a957b5714794807c05f2601ff7ca59
Nick Kruckenberg <nkruckenberg@gmail.com>
2020-09-20 07:27:28 -0400

    Working with dummied cache and dummied db return -- returning in correct order and not losing data from cache.

--- quell-server/src/quell.js
+++ quell-server/src/quell.js
@@ -39,7 +39,7 @@ class QuellCache {
   }
 
   
-  async query(req, res, next) {
+  query(req, res, next) {
     // handle request without query
     if (!req.body.query) {
       return next('Error: no GraphQL query found on request body');
@@ -57,44 +57,61 @@ class QuellCache {
     if (proto === 'error') {
       return next('Error: Quell currently only supports GraphQL queries');
     }
-
+    
     const queryName = Object.keys(proto)[0];
     const queriedCollection = this.queryMap[queryName];
-
+    
+    
     // build response from cache
     const responseFromCache = this.buildFromCache(proto, this.queryMap, queriedCollection);
-
+       
+    
     let fullResponse;
-    const queryName = Object.keys(proto)[0];
 
     if (responseFromCache.length === 0) {
-      fullResponse = await this.graphQLHandoff(queryString);
+      fullResponse = this.graphQLHandoff(queryString);
+      this.cache(fullResponse, queriedCollection, queryName);
     } else {
-      const queryObject = createQueryObj(proto);
+      const queryObject = this.createQueryObj(proto);
       
       if (Object.keys(queryObject).length > 0) {
-        const newQueryString = createQueryStr(queryObject);
-        const uncachedResponse = await this.graphQLHandoff(newQueryString);
-        fullResponse = joinResponses(responseFromCache, uncachedResponse);
+        const newQueryString = this.createQueryStr(queryObject);
+        const uncachedResponse = this.graphQLHandoff(newQueryString);
+        fullResponse = this.joinResponses(responseFromCache, uncachedResponse);
+        this.cache(fullResponse, queriedCollection, queryName);
       } else {
         fullResponse = responseFromCache;
       }
     }
 
-    cache({ [queryName]: fullResponse);
 
-    res.locals.queryResponse = { data: { [queryName]: fullResponse }};
-    return next();
+    // res.locals.queryResponse = { data: { [queryName]: fullResponse }};
+    // return next();
+    
+    const freshProto = this.parseAST(AST);
+
+    const refetchedFromCache = this.buildFromCache(freshProto, this.queryMap, queriedCollection);
+
+    // return fullResponse;
+    return refetchedFromCache;
   };
 
-  async graphQLHandoff(query) {
-    graphql(this.schema, query)
-      .then((results) => {
-        return results;
-      })
-      .catch((error) => {
-        return `Error in graphQLHandoff: ${error}`
-      });
+  graphQLHandoff(query) {
+    // graphql(this.schema, query)
+    //   .then((results) => {
+    //     return results;
+    //   })
+    //   .catch((error) => {
+    //     return `Error in graphQLHandoff: ${error}`
+    //   });
+
+    return ([
+      { 'capital': 'Andorra la Vella', 'cities': [{ 'population': 1052 }, { 'population': 7211 }] },
+      { 'capital': 'Sucre', 'cities': [{ 'population': 4013 }, { 'population': 5157 }] },
+      { 'capital': 'Yerevan', 'cities': [{ 'population': 3292 }, { 'population': 22233 }] },
+      { 'capital': 'Pago Pago', 'cities': [{ 'population': 5157 }, { 'population': 20430 }] },
+      { 'capital': 'Oranjestad', 'cities': [{ 'population': 4715 }, { 'population': 0 }] }
+    ])
   };
 
   /**
@@ -256,7 +273,7 @@ class QuellCache {
     const response = [];
     
     for (const superField in proto) {
-      if (!collection) collection = JSON.parse(dummyCache.get(map[superField])) || [];
+      if (!collection) collection = JSON.parse(dummyCache.get(superField)) || [];
       if (collection.length === 0) this.toggleProto(proto);
       for (const item of collection) {
         response.push(this.buildItem(proto[superField], this.fieldsMap[queriedCollection], JSON.parse(dummyCache.get(item))));
@@ -321,6 +338,9 @@ class QuellCache {
   };
 
   queryStringify(fieldsArray) {
+    const openCurl = ' { ';
+    const closedCurl = ' } ';
+    
     let innerStr = '';
     for (let i = 0; i < fieldsArray.length; i += 1) {
       if (typeof fieldsArray[i] === 'string') {
@@ -335,7 +355,90 @@ class QuellCache {
     return innerStr;
   };
 
+  joinResponses(cachedArray, uncachedArray) {
+    const joinedArray = [];
+
+    for (let i = 0; i < uncachedArray.length; i += 1) {
+      joinedArray.push(this.recursiveJoin(cachedArray[i], uncachedArray[i]));
+    }
+
+    return joinedArray;
+  };
+
+  recursiveJoin(cachedItem, uncachedItem) {
+    const joinedObject = cachedItem || {};
+    
+    for (const field in uncachedItem) {
+      if (Array.isArray(uncachedItem[field])) {
+        if (typeof uncachedItem[field][0] === 'string') {
+          const temp = [];
+          for (let reference of uncachedItem[field]) {
+            temp.push(JSON.parse(dummyCache.get(reference)))
+          }
+          uncachedItem[field] = temp;
+        }
+        if (cachedItem[field]) {
+          
+          joinedObject[field] = this.joinResponses(cachedItem[field], uncachedItem[field]);
+        } else {
+          joinedObject[field] = uncachedItem[field];
+        }
+      } else {
+        joinedObject[field] = uncachedItem[field];
+      }
+    }
+    return joinedObject;
+  };
+
+  generateId(collection, item) {
+    const identifier = item.id || item._id || 'uncacheable';    
+    return collection + '-' + identifier.toString();
+  };
   
+  writeToCache(key, item) {
+    if (!key.includes('uncacheable')) {
+      dummyCache.set(key, JSON.stringify(item));
+    }
+  };
+  
+  replaceItemsWithReferences(queryName, field, array) {
+    const arrayOfReferences = [];
+    const typeQueried = this.queryMap[queryName];
+    const collectionName = this.fieldsMap[typeQueried][field];
+  
+    for (const item of array) {
+      this.writeToCache(this.generateId(collectionName, item), item);
+      arrayOfReferences.push(this.generateId(collectionName, item));
+    }
+  
+    return arrayOfReferences;
+  };
+
+  cache(response, collectionName, queryName) {
+    const collection = JSON.parse(JSON.stringify(response));
+  
+    const referencesToCache = [];
+  
+    // Check for nested array (to replace objects with another array of references)
+    for (const item of collection) {
+      
+      const cacheId = this.generateId(collectionName, item);
+      const joinedWithCache = this.recursiveJoin(item, JSON.parse(dummyCache.get(cacheId)));
+      const itemKeys = Object.keys(joinedWithCache);
+      
+      for (const key of itemKeys) {
+        if (Array.isArray(joinedWithCache[key])) {
+          joinedWithCache[key] = this.replaceItemsWithReferences(queryName, key, joinedWithCache[key]);
+        }
+      }
+      // Write individual objects to cache (e.g. separate object for each single city)
+      this.writeToCache(cacheId, joinedWithCache);
+      referencesToCache.push(cacheId);
+    }
+    // Write the array of references to cache (e.g. 'City': ['City-1', 'City-2', 'City-3'...])
+    this.writeToCache(queryName, referencesToCache);
+  };
+
 };
 
 const quell = new QuellCache(mockSchema, 1000, 1000);
@@ -343,7 +446,7 @@ const quell = new QuellCache(mockSchema, 1000, 1000);
 // console.log('fields map:  ', quell.fieldsMap);
 // console.log('proto:   ', quell.parseAST(parse(mockQuery)));
 
-const fakeData = {
+const fakeDataComplete = {
   'Country': ['Country-1', 'Country-2', 'Country-3', 'Country-4', 'Country-5'],
   'City': ['City-1', 'City-2', 'City-3', 'City-4', 'City-5', 'City-6', 'City-7', 'City-8','City-9', 'City-10'],
   'Country-1': {'id': 1, 'name': 'Andorra', 'capital': 'Andorra la Vella', 'cities': ['City-1', 'City-2']},
@@ -363,11 +466,50 @@ const fakeData = {
   'City-10': {"id":10,"country_id":5,"name":"Villa Serrano","population":0}
 };
 
-for (const key in fakeData) {
-  dummyCache.set(key, JSON.stringify(fakeData[key]));
+const fakeDataPartial = {
+  'countries': ['Country-1', 'Country-2', 'Country-3', 'Country-4', 'Country-5'],
+  'cities': ['City-1', 'City-2', 'City-3', 'City-4', 'City-5', 'City-6', 'City-7', 'City-8','City-9', 'City-10'],
+  'Country-1': {'id': 1, 'name': 'Andorra', 'cities': ['City-1', 'City-2']},
+  'Country-2': {'id': 2, 'name': 'Bolivia', 'cities': ['City-5', 'City-7']},
+  'Country-3': {'id': 3, 'name': 'Armenia', 'cities': ['City-3', 'City-6']},
+  'Country-4': {'id': 4, 'name': 'American Samoa', 'cities': ['City-8', 'City-4']},
+  'Country-5': {'id': 5, 'name': 'Aruba', 'cities': ['City-9', 'City-10']},
+  'City-1': {"id": 1, "country_id": 1, "name": "El Tarter"},
+  'City-2': {"id": 2,"country_id": 1, "name": "La Massana"},
+  'City-3': {"id":3,"country_id":3,"name":"Canillo"},
+  'City-4': {"id":4,"country_id":4,"name":"Andorra la Vella"},
+  'City-5': {"id":5,"country_id":2,"name":"Jorochito"},
+  'City-6': {"id":6,"country_id":3,"name":"Tupiza"},
+  'City-7': {"id":7,"country_id":2,"name":"Puearto Pailas"},
+  'City-8': {"id":8,"country_id":4,"name":"Capinota"},
+  'City-9': {"id":9,"country_id":5,"name":"Camargo"},
+  'City-10': {"id":10,"country_id":5,"name":"Villa Serrano"}
 };
-console.log(quell.query({body: { query: "{countries{id name cities { id name }}}" }}));
+
+const dataFromResolvers = [
+  { 'capital': 'Andorra la Vella', 'cities': [{ 'population': 1052 }, { 'population': 7211 }] },
+  { 'capital': 'Sucre', 'cities': [{ 'population': 4013 }, { 'population': 5157 }] },
+  { 'capital': 'Yerevan', 'cities': [{ 'population': 3292 }, { 'population': 22233 }] },
+  { 'capital': 'Pago Pago', 'cities': [{ 'population': 5157 }, { 'population': 20430 }] },
+  { 'capital': 'Oranjestad', 'cities': [{ 'population': 4715 }, { 'population': 0 }] }
+];
+
+for (const key in fakeDataPartial) {
+  dummyCache.set(key, JSON.stringify(fakeDataPartial[key]));
+};
+console.log(quell.query({body: { query: "{countries{id name capital cities { id name population }}}" }}));
+// console.log(quell.query({body: { query: "{countries{id capital cities { id name population }}}" }}));
+
+// const cached = [
+//   { name: 'George', id: 1, human: true, friends: [ { name: 'John', human: true }, { name: 'Wesley', human: true }] },
+//   { name: 'Luke', id: 2, human: false, friends: [{ name: 'George', human: true }, { name: 'Jim', human: true }] },
+// ];
+
+// const uncached = [
+//   { hometown: 'Newport', sports: ['hockey', 'lacrosse'], friends: [ { name: 'NoOne', human: true }, { name: 'YouKnow', human: false }, { name: 'NewFriend', human: true }] },
+//   { hometown: 'Charleston', sports: ['baseball'] },
+// ]
+
+// console.log(quell.joinResponses(cached, uncached)[0].friends);
 
 module.exports = QuellCache;
-
-// {query: "{countries{id name capital cities{ id name }}}"}
