b5db4f4a74de6552beefa30c17e1dc1966dd574a
MichaelLauri <64538807+MichaelLauri@users.noreply.github.com>
2020-09-22 12:40:13 -0400

    Add demo instructions, remove Tooltip

--- test-site/client/src/components/Metrics.jsx
+++ test-site/client/src/components/Metrics.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import Tooltip from '@material-ui/core/Tooltip';
 
-
-
-
 const Metrics = (props) => {
   const { fetchTime, cacheStatus } = props;
-  return(
-    <div className="metrics-div">
+  return (
+    <div className='metrics-div'>
       <h3>Metrics:</h3>
-      <div className="metrics-grid">
-        <div className="timer-div">
-          <Tooltip title="Information about this component goes here.">
-            <div className="metric-value">{fetchTime}</div>
-          </Tooltip>
-          <div className="metric-label">Cache/Fetch Time</div>
+      <div className='metrics-grid'>
+        <div className='timer-div'>
+          <div className='metric-value'>{fetchTime}</div>
+
+          <div className='metric-label'>Cache/Fetch Time</div>
         </div>
       </div>
-      <div className="cache-cleared-div">Cache Cleared: {cacheStatus}</div>
+      <div className='cache-cleared-div'>Cache Cleared: {cacheStatus}</div>
     </div>
-  )
-}
+  );
+};
 
 export default Metrics;
