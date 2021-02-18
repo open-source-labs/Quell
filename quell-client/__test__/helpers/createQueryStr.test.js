4334ab10fb805eda129019c9c373ec43752edf19
Xiao Yu Omeara <63607193+xyomeara@users.noreply.github.com>
2021-02-17 21:31:29 -0500

    set up quell-client side testing files

---
+++ quell-client/__test__/helpers/createQueryStr.test.js
@@ -0,0 +1,8 @@
+const createQueryStr = require('../../helpers/createQueryStr');
+
+describe('createQueryStr', () => {
+  test.only('should return an array of strings and numbers', () => {
+    expect(createQueryStr()).toEqual();
+    expect(createQueryStr()).toEqual();
+  });
+});
