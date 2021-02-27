b2e2940e272fd502b9fa736f65d537473fb55475
Xiao Yu Omeara <63607193+xyomeara@users.noreply.github.com>
2021-02-27 09:56:20 -0500

    update

---
+++ quell-client/__test__/Quellify.test.js
@@ -0,0 +1,7 @@
+const Quellify = require('../Quellify');
+
+describe('Quellify.test.js', () => {
+  test.skip('Quellify.test.js', () => {
+    expect(Quellify()).toEqual({});
+  });
+});
---
+++ quell-client/__test__/helpers/buildFromCache.test.js
@@ -0,0 +1,7 @@
+const buildFromCache = require('../../helpers/buildFromCache');
+
+describe('buildFromCache.test.js', () => {
+  test.skip('buildFromCache.test.js', () => {
+    expect(buildFromCache()).toEqual({});
+  });
+});
