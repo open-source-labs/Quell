a1d33a657d5b03e845e9a55a1150c44a6b43ec74
Thomas Reeder <thomaseugenereeder@gmail.com>
2021-06-12 23:40:06 -0700

    parseAST sort of functioning

--- quell-client/src/helpers/parseAST.js
+++ quell-client/src/helpers/parseAST.js
@@ -1,10 +1,9 @@
 const { visit, BREAK } = require('graphql/language/visitor');
 const { parse } = require('graphql/language/parser');
 
-// TO-DO: refactor prototype to put alias / fieldname first
-// avoid aliased prototypes overwriting one-another
 
-/** TO-DO: refactor prototype to include args
+/** TO-DO: refactor prototype to include args & put alias first
+ * CURR: messes up inner-args
  * `${fieldName}${ID ? '-' + ID : ''}`
  * Country-ID
 {
@@ -24,15 +23,15 @@ const { parse } = require('graphql/language/parser');
 const parseAST = (AST) =>{
   // initialize prototype as empty object
   const prototype = {};
-  //let isQuellable = true;
 
+  // initialize operation Type, will store "query", "mutation", etc.
+  // TO-DO: is there space to merge this with prototype?
   let operationType;
 
-  // initialiaze arguments as null
-  let protoArgs = null; //{ country: { id: '2' } }
 
   // initialize stack to keep track of depth first parsing
   const stack = [];
+  let fieldID = '';
 
   /**
    * visit is a utility provided in the graphql-JS library. It performs a
@@ -61,122 +60,127 @@ const parseAST = (AST) =>{
     },
     Field: {
       enter(node) {
+        console.log('enter field');
         if (node.alias && !node.arguments) {
         // TO-DO: handle edge case of alias without any arguments
           operationType = 'unQuellable';
           return BREAK;
         }
         if (node.arguments && node.arguments.length > 0) {
-          protoArgs = protoArgs || {};
-          // console.log('node', node);
-          protoArgs[node.name.value] = {};
-
-          // initializes args object in format { Alias: {fieldName: country, id: 1}}
-          // if no alias, stores on key of fieldName
-          // ID will be placed on this object later
-          if (node.alias) {
-            protoArgs[node.alias.value] = { fieldName: node.name.value };
-          } else {
-            protoArgs[node.name.value] = { fieldName: node.name.value };
-          }
-
-          // TO-DO: replace protoArgs
-
-          // collect arguments if arguments contain id, otherwise make query unquellable
-          // hint: can check for graphQl type ID instead of string 'id'
-          for (let i = 0; i < node.arguments.length; i++) {
-            const key = node.arguments[i].name.value;
-            const value = node.arguments[i].value.value;
-
-            // for queries cache can handle only id as argument
-            if (operationType === 'query') {
-              if (!key.includes('id')) {
-                operationType = 'unQuellable';
-                return BREAK;
-              }
+          // populates argsObj from node
+          const argsObj = {};
+          node.arguments.forEach(arg => {
+            // TO-DO: cannot currently handle variables in query
+            if (arg.value.kind === 'Variable' && operationType === 'query') {
+              operationType = 'unQuellable';
+              return BREAK;
             }
-            // TO-DO update for new protoArgs
-            protoArgs[node.name.value][key] = value;
+            argsObj[arg.name.value] = arg.value.value;
+          });
+
+          // identify unique ID from args
+          // TO-DO: could expand to support user-defined IDs (ie key.includes('id') for "authorid")
+          let uniqueID = '';
+          for (const key in argsObj) {
+            if (key === 'id' || key === '_id' || key === 'ID') {
+              uniqueID = argsObj[key];
+            }
           }
+
+          // require uniqueID in order to be cached
+          // 
+          if (!uniqueID) {
+            operationType = 'unQuellable';
+            return BREAK;
+          }
+
+          // create fieldName unique ID in format "fieldName - uniqueID"
+          const fieldNameID = `${node.name.value}${uniqueID ? '-' + uniqueID : null}`
+      
+          // add args to prototype object
+          prototype[fieldNameID] = { ...prototype[fieldNameID], __args: argsObj }
+
+          // check for Alias and add if it exists, otherwise set to null
+          if (node.alias) prototype[fieldNameID] = { ...prototype[fieldNameID], __alias: node.alias.value }
+          else prototype[fieldNameID] = {...prototype[fieldNameID], __alias: null }
+
+          // TO-DO refactor to be one variable
+          // store in outer field name
+          fieldID = fieldNameID;
         }
         // add value to stack
         stack.push(node.name.value);
       },
       leave(node) {
+        console.log('leave node');
         // remove value from stack
         stack.pop();
       },
     },
     SelectionSet(node, key, parent, path, ancestors) {
+      console.log('enter selectionset');
       /* Exclude SelectionSet nodes whose parents' are not of the kind
        * 'Field' to exclude nodes that do not contain information about
        *  queried fields.
        */
       if (parent.kind === 'Field') {
         // loop through selections to collect fields
-        const tempObject = {};
+        const fieldsObject = {};
         for (let field of node.selections) {
-          tempObject[field.name.value] = true;
-        }
+          fieldsObject[field.name.value] = true;
+        };
 
         // loop through stack to get correct path in proto for temp object;
-        // mutates original prototype object;
+        // mutates original prototype object WITH values from tempObject
+        // "prev" is accumulator ie the prototype
         const protoObj = stack.reduce((prev, curr, index) => {
+          console.log(stack);
+          console.log(curr);
+          console.log(prev);
           return index + 1 === stack.length // if last item in path
-            ? (prev[curr] = tempObject) // set value
-            : (prev[curr] = prev[curr]); // otherwise, if index exists, keep value
+            ? (stack.length > 1 ? prev[curr] = fieldsObject : prev[fieldID] = { ...prev[fieldID], ...fieldsObject }) //set value
+            : (prev[fieldID] = prev[fieldID]); // otherwise, if index exists, keep value
         }, prototype);
       }
     },
   });
-  return { prototype, protoArgs, operationType };
+  return { prototype, operationType };
 }
 
-
-// const query = `query {
-//   Canada: country (id: 1) {
-//     id
-//     name
-//     population
-//   }
-//   Mexico: country (id: 2) {
-//     id
-//     name
-//     capitol
-//     cities {
-//       id
-//       name
-//     }
-//   }
-// }`;
-
-// ${ fieldName }${ ID ? '-'+ID : null } {
-//   id
-//   name
-//   capitol
-// }
-
-// {
-//   __alias: alias
-//   __arg: arg
-//   id: true
-//   name: true
-// }
+const query = `query {
+  Canada: country (id: 1) {
+    id
+    name
+    food (id: 3) {
+      name
+    }
+  }
+  Mexico: country (id: 2) {
+    id
+    name
+    capitol
+    cities {
+      id
+      name
+    }
+  }
+}`;
 
 // loop(keys){
 //   if (!key.includes('__')) {
 //     // building prototype stuff
 //   }
-// // }
+// }
 
-// const query2 = `{countries { id name capitol } }`;
+const query2 = `{countries { id name capitol } }`;
 
-// const parsedQuery = parse(query);
-// const { prototype, protoArgs, operationType } = parseAST(parsedQuery);
+const parsedQuery = parse(query2);
+const { prototype, operationType } = parseAST(parsedQuery);
 
 // console.log('query', query);
-// console.log('proto', prototype);
-// console.log('protoArgs', protoArgs);
+console.log('proto', prototype);
+// console.log('country-1', prototype['country-1']);
+// console.log('country-2', prototype['country-2']);
 // console.log('opType', operationType);
 
 // for (let query2 in prototype) {
