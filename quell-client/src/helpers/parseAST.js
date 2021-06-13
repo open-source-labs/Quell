48a1d7316429a320038a587b110a41f8dcc67dd3
Thomas Reeder <thomaseugenereeder@gmail.com>
2021-06-13 10:30:12 -0700

    parseAST now properly accounts for mulitiple queries, arguments, aliases, code needs to be cleaned up & tightened

--- quell-client/src/helpers/parseAST.js
+++ quell-client/src/helpers/parseAST.js
@@ -20,17 +20,22 @@ const { parse } = require('graphql/language/parser');
  * representing all the queried fields nested as they are in the query.
  */
 
-const parseAST = (AST) =>{
+const parseAST = (AST) => {
   // initialize prototype as empty object
   const prototype = {};
 
   // initialize operation Type, will store "query", "mutation", etc.
   // TO-DO: is there space to merge this with prototype?
-  let operationType;
 
 
-  // initialize stack to keep track of depth first parsing
+  // initialize stack to keep track of depth first parsing path
   const stack = [];
+  const stackIDs = [];
+
+  // tracks depth of selection Set
+  let selectionSetDepth = 0;
+
+  // keep track of current fieldID
   let fieldID = '';
 
   /**
@@ -44,40 +49,43 @@ const parseAST = (AST) =>{
    */
   visit(AST, {
     enter(node) {
+      //TO-DO: cannot cache directives, return as unquellable until support
       if (node.directives) {
         if (node.directives.length > 0) {
-          operationType = 'unQuellable';
+          prototype.operationType = 'unQuellable';
           return BREAK;
         }
       }
     },
     OperationDefinition(node) {
-      operationType = node.operation;
-      if (node.operation === 'subscription') {
-        operationType = 'unQuellable';
+      //TO-DO: cannot cache subscriptions or mutations, return as unquellable
+      prototype.operationType = node.operation;
+      if (node.operation === 'subscription' || node.operation === 'mutation') {
+        prototype.operationType = 'unQuellable';
         return BREAK;
       }
     },
     Field: {
-      enter(node) {
-        console.log('enter field');
+      // enter the node to construct a unique field-ID for critical fields
+      enter(node, key, parent, path, ancestors) {
         if (node.alias && !node.arguments) {
-        // TO-DO: handle edge case of alias without any arguments
-          operationType = 'unQuellable';
-          return BREAK;
+          // TO-DO: handle edge case of alias without any arguments
+            prototype.operationType = 'unQuellable';
+            return BREAK;
         }
-        if (node.arguments && node.arguments.length > 0) {
+        // TO-DO: re-implement "if argument" statement
+        if (true) {
           // populates argsObj from node
           const argsObj = {};
           node.arguments.forEach(arg => {
             // TO-DO: cannot currently handle variables in query
-            if (arg.value.kind === 'Variable' && operationType === 'query') {
-              operationType = 'unQuellable';
+            if (arg.value.kind === 'Variable' && prototype.operationType === 'query') {
+              prototype.operationType = 'unQuellable';
               return BREAK;
             }
             argsObj[arg.name.value] = arg.value.value;
           });
-
+  
           // identify unique ID from args
           // TO-DO: could expand to support user-defined IDs (ie key.includes('id') for "authorid")
           let uniqueID = '';
@@ -86,81 +94,149 @@ const parseAST = (AST) =>{
               uniqueID = argsObj[key];
             }
           }
-
+  
           // require uniqueID in order to be cached
           // 
-          if (!uniqueID) {
-            operationType = 'unQuellable';
-            return BREAK;
-          }
-
+          // if (!uniqueID) {
+          //   operationType = 'unQuellable';
+          //   return BREAK;
+          // }
+  
           // create fieldName unique ID in format "fieldName - uniqueID"
-          const fieldNameID = `${node.name.value}${uniqueID ? '-' + uniqueID : null}`
-      
-          // add args to prototype object
-          prototype[fieldNameID] = { ...prototype[fieldNameID], __args: argsObj }
+          fieldID = `${node.name.value}${uniqueID ? '--' + uniqueID : ''}`
 
-          // check for Alias and add if it exists, otherwise set to null
-          if (node.alias) prototype[fieldNameID] = { ...prototype[fieldNameID], __alias: node.alias.value }
-          else prototype[fieldNameID] = {...prototype[fieldNameID], __alias: null }
+          if (uniqueID) {
+            if (selectionSetDepth >= 1) {
+              console.log('deep uniqueID', fieldID, selectionSetDepth);
+              // when depth is > 1, we need to add it to parent object instead of prototype-base
+              const finalIndex = stack.length - 1;
+              const penultimateIndex = stack.length - 2;
+              console.log('deep', stackIDs)
+              const parentObj = stackIDs[finalIndex]
+              const currentObj = fieldID;
 
-          // TO-DO refactor to be one variable
-          // store in outer field name
-          fieldID = fieldNameID;
+              console.log('deep parent', parentObj);
+              console.log('deep current', currentObj);
+              console.log('deep proto', prototype);
+
+              // add args to prototype object
+              prototype[parentObj][currentObj] = { ...prototype[parentObj][currentObj], __args: argsObj }
+    
+              // // check for Alias and add if it exists, otherwise set to null
+              if (node.alias) prototype[parentObj][currentObj] = { ...prototype[parentObj][currentObj], __alias: node.alias.value }
+              else prototype[parentObj][currentObj] = { ...prototype[parentObj][currentObj], __alias: null }
+
+            } else {
+              console.log('not deep uniqueID', fieldID, selectionSetDepth);
+              // add args to prototype object
+              prototype[fieldID] = { ...prototype[fieldID], __args: argsObj }
+    
+              // check for Alias and add if it exists, otherwise set to null
+              if (node.alias) prototype[fieldID] = { ...prototype[fieldID], __alias: node.alias.value }
+              else prototype[fieldID] = { ...prototype[fieldID], __alias: null }
+            }
+          }
         }
-        // add value to stack
+
+        // add value to stack to keep track of depth-first parsing path
         stack.push(node.name.value);
+        stackIDs.push(fieldID);
+        // console.log('stack', stack);
+        // console.log('stackIDs', stackIDs);
       },
-      leave(node) {
-        console.log('leave node');
-        // remove value from stack
+      leave(node, key, parent, path, ancestors) {
         stack.pop();
+        stackIDs.pop();
+        console.log('leaving stack', stack);
       },
     },
-    SelectionSet(node, key, parent, path, ancestors) {
-      console.log('enter selectionset');
+    SelectionSet: {
+      // selection sets contain all of the sub-fields
+      // iterate through the sub-fields to construct fieldsObject
+      enter(node, key, parent, path, ancestors) {
+        console.log('enter SS', node.kind, selectionSetDepth);
+
+        if (parent.kind === 'Field') {
+          selectionSetDepth++;
+          // console.log('parent Field depth', selectionSetDepth);
+          // console.log('stack', stack);
+          // console.log('stackIDs', stackIDs);
+          
+          // TO-DO: this adds "city" to the object when I want "city--3"
+          // loop through selections to collect fields
+          const fieldsObject = {};
+          for (let field of node.selections) {
+            fieldsObject[field.name.value] = true;
+          };
+  
+
+          // if selection set depth > 1, we can remove the duplicate ID that exists at top of stack from the object, replace it with fieldsObject
+          // otherwise proto-object will have duplicate entries for nested queries with arguments
+          if (selectionSetDepth > 1) {
+            const finalIndex = stack.length - 1;
+            const penultimateIndex = stack.length - 2;
+
+            delete prototype[stackIDs[penultimateIndex]][stack[finalIndex]];
+
+            stackIDs.reduce((prev, curr, index) => {
+              return index + 1 === stack.length // if last item in path
+                ? (prev[curr] = {...prev[curr], ...fieldsObject}) //set value
+                : (prev[curr] = prev[curr]); // otherwise, if index exists, keep value
+            }, prototype);
+          } else {
+            // loop through stack to get correct path in proto for temp object;
+            // mutates original prototype object WITH values from tempObject
+            // "prev" is accumulator ie the prototype
+            stackIDs.reduce((prev, curr, index) => {
+              return index + 1 === stack.length // if last item in path
+                ? (prev[curr] = fieldsObject) //set value
+                : (prev[curr] = prev[curr]); // otherwise, if index exists, keep value
+            }, prototype);
+          }
+
+          console.log('proto before reduce', prototype);
+        }
+      },
       /* Exclude SelectionSet nodes whose parents' are not of the kind
        * 'Field' to exclude nodes that do not contain information about
        *  queried fields.
        */
-      if (parent.kind === 'Field') {
-        // loop through selections to collect fields
-        const fieldsObject = {};
-        for (let field of node.selections) {
-          fieldsObject[field.name.value] = true;
-        };
-
-        // loop through stack to get correct path in proto for temp object;
-        // mutates original prototype object WITH values from tempObject
-        // "prev" is accumulator ie the prototype
-        const protoObj = stack.reduce((prev, curr, index) => {
-          console.log(stack);
-          console.log(curr);
-          console.log(prev);
-          return index + 1 === stack.length // if last item in path
-            ? (stack.length > 1 ? prev[curr] = fieldsObject : prev[fieldID] = { ...prev[fieldID], ...fieldsObject }) //set value
-            : (prev[fieldID] = prev[fieldID]); // otherwise, if index exists, keep value
-        }, prototype);
-      }
+      leave(node, key, parent, path, ancestors) {
+        console.log('leaving SS', node.kind, selectionSetDepth);
+        selectionSetDepth--;
+      },
     },
   });
-  return { prototype, operationType };
-}
+  return prototype;
+};
 
 const query = `query {
   Canada: country (id: 1) {
     id
     name
-    food (id: 3) {
+    food
+    city (id: 3) {
       name
     }
   }
   Mexico: country (id: 2) {
     id
     name
-    capitol
-    cities {
+    city {
       id
+      name
+    }
+  }
+}`;
+
+const query1 = `query {
+  Canada: country (id: 1) {
+    id
+    name
+    food {
+      flavor
+    }
+    city (id: 3) {
       name
     }
   }
@@ -174,17 +250,13 @@ const query = `query {
 
 const query2 = `{countries { id name capitol } }`;
 
-const parsedQuery = parse(query2);
-const { prototype, operationType } = parseAST(parsedQuery);
+const query3 = `{countries { id name cities { id name } } }`
+
+const parsedQuery = parse(query);
+const prototype = parseAST(parsedQuery);
 
 // console.log('query', query);
 console.log('proto', prototype);
-// console.log('country-1', prototype['country-1']);
-// console.log('country-2', prototype['country-2']);
-// console.log('opType', operationType);
-
-// for (let query2 in prototype) {
-//   console.log('for query in proto', query2);
-// }
+// console.log('depth proto', prototype['country--1']['city--3']);
 
 module.exports = parseAST;
