8cd96eb0f8114335b0f9bef655ded15a182d10e3
Thomas Reeder <thomaseugenereeder@gmail.com>
2021-06-28 15:36:13 -0700

    tweaks to server code & removing unnecessary dependencies

--- demo/server/server.js
+++ demo/server/server.js
@@ -9,7 +9,7 @@ const QuellCache = require(graphqlNodeModule);
 
 // Express instance
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Instantiate cache GraphQL middleware
 const redisPort =
