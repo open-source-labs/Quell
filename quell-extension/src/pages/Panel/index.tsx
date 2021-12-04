301866abc939a38a57cceb857c573bb0f21e902b
Robert Howton <robert.f.howton@gmail.com>
2021-12-03 19:16:45 -0500

    merging extension updates into quell dev branch

--- quell-extension/src/pages/Panel/index.tsx
+++ quell-extension/src/pages/Panel/index.tsx
@@ -2,14 +2,9 @@ require('file-loader?name=[name].[ext]!./index.html');
 import ReactDOM from 'react-dom';
 import App from './App';
 import './global.scss';
-import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-
-const theme = createMuiTheme({
-  palette: {
-    type: 'dark',
-  }
-});
+import theme from './theme';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
@@ -18,7 +13,3 @@ ReactDOM.render(
   </ThemeProvider>,
   document.getElementById('app')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
