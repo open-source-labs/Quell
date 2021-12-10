64dae630f8b6619be372d16ecc05b1678a09aff7
Robert Howton <robert.f.howton@gmail.com>
2021-12-10 10:46:46 -0500

    removing obsolete extension

--- quell-extension.bak/src/pages/Devtools/index.tsx
+++
@@ -1,7 +0,0 @@
-// Leave this alone - simply to create a tab in Chrome DevTools
-
-chrome.devtools.panels.create(
-  'Quell', //input dev tool name
-  null, //icon for the dev tool if any - may use null
-  'panel.html' //panel code
-);
