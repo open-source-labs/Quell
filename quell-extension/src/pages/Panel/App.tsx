03db2ec86e960332e8b95dc2f6046fadf432d542
Robert Howton <robert.f.howton@gmail.com>
2021-12-13 09:50:37 -0500

    cleaned up settings page

--- quell-extension/src/pages/Panel/App.tsx
+++ quell-extension/src/pages/Panel/App.tsx
@@ -52,10 +52,10 @@ const App = () => {
   };
 
   // COMMENT OUT IF WORKING FROM DEV SERVER
-  // useEffect(() => {
-  //   handleRequestFinished(gqlListener);
-  //   handleNavigate(gqlListener);
-  // }, []);
+  useEffect(() => {
+    handleRequestFinished(gqlListener);
+    handleNavigate(gqlListener);
+  }, []);
 
   useEffect(() => {
     const introspectionQuery = getIntrospectionQuery();
