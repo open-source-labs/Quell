f00c80078a0c965fa5137ecdaf04c9df1dff6722
David Lopez <d7lopez@gmail.com>
2022-01-22 09:26:41 -0600

    pulling down latest version

--- test-app/server/serverGQL.js
+++ test-app/server/serverGQL.js
@@ -11,7 +11,7 @@ const schema = require('./schemas/quellSchemas')
 const app = express();
 const redisPort = 6379
 const PORT = 3434;
-const quellCache = new QuellCache(schema, redisPort, 1200);
+// const quellCache = new QuellCache(schema, redisPort, 1200);
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'../assets')));
