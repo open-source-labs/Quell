f9d37435b8bc3d3a5c2e70fb772cb829247459b9
Jonah <jonahpageweinbaum@gmail.com>
2023-05-21 00:44:15 -0700

    Add request listener to parse responses containing query times

--- quell-extension/src/pages/Panel/App.tsx
+++ quell-extension/src/pages/Panel/App.tsx
@@ -25,6 +25,7 @@ const App = () => {
   const [results, setResults] = useState({});
   const [schema, setSchema] = useState({});
   const [queryString, setQueryString] = useState<string>("");
+  const [queryTimes, setQueryTimes] = useState([]);
   const [clientRequests, setClientRequests] = useState<ClientRequest[]>([]);
 
   // various routes to get information
@@ -56,11 +57,32 @@ const App = () => {
       });
     }
   };
+
+  // function to listen to network requests and add query times to state
+  const timeListener = (request: ClientRequest): void => {
+    // if request was sent to the /api/queryTime route, add response body to queryTimes state
+    request.getContent((body) => {
+      const responseData = JSON.parse(body);
+      console.log("responseData: ", responseData);
+      if (responseData.time) {
+        setQueryTimes((prev) => prev.concat([responseData.time]));
+      }
+    });
+  };
+
+  // TODO: remove
+  // useffect to test if setQueryTimes is working
+  useEffect(() => {
+    console.log("queryTimes: ", queryTimes);
+  }, [queryTimes]);
   
   // COMMENT OUT IF WORKING FROM DEV SERVER
   useEffect(() => {
     handleRequestFinished(gqlListener);
     handleNavigate(gqlListener);
+
+    handleRequestFinished(timeListener);
+    handleNavigate(timeListener);
   }, []);
 
   useEffect(() => {
