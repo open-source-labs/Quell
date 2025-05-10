024b844a37e107b63b91a01be0ae6b7f54c60dfb
Jeremy Dalton <jeremydalton91@gmail.com>
2025-05-10 13:53:29 -0400

    Further refined scope of coverage within quell-server

--- quell-server/jest.config.js
+++ quell-server/jest.config.js
@@ -2,8 +2,19 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ["dist"],
-  forceCoverageMatch: ['**/*.test.(ts|js)'],
   collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!**/__tests__/**',
+    '!**/*.test.{ts,tsx}',
+    '!dist/**',
+  ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/__tests__/',
+    '\\.test\\.(ts|tsx)$',
+  ],
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.json',
