5a6a47833aec8964c2c4a312598a931f3fc6e3b3
dsriva03 <dsriva03@gmail.com>
2025-05-19 21:25:46 -0400

    refactor: use createServerError helper in rateLimiter func

--- quell-server/src/helpers/cacheUtils.ts
+++ quell-server/src/helpers/cacheUtils.ts
@@ -1,7 +1,23 @@
 // src/helpers/cacheUtils.ts
 import { ProtoObjType, QueryMapType } from "../types";
+import { ServerErrorType } from "../types";
 
 export function getCacheID(proto: ProtoObjType, map: QueryMapType): string {
   const base = map[proto.__type as string] ?? proto.__type;
   return proto.__id ? `${base}--${proto.__id}` : String(base);
 }
+
+export function createServerError(
+    log: string,
+    status = 400,
+    userMessage?: string
+  ): ServerErrorType {
+    return {
+      log,
+      status,
+      message: {
+        err: userMessage ?? "An error occurred. Check server logs.",
+      },
+    };
+  }
+  
